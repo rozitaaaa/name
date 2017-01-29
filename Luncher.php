0f274d200f47321b18fa4184ae1dc1cb950eb378
rozitaaaa <rozitazakere@gmail.com>
2017-01-29 19:46:58 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,6 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('
+329743459:AAHgeZ0mQhGdjXJVZ1EeamjIpbILtHmdO2c');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
